7af8e9c01859d008ac5144d6d4039fa05ed1b145
Ilho Kim <isagum@gmail.com>
2019-02-06 16:16:52 -0800

    feat: create Animal abstract class

---
+++ src/com/lambdaschool/Animal.java
@@ -0,0 +1,12 @@
+package com.lambdaschool;
+
+public abstract class Animal {
+    int idNumber;
+    String name;
+    int yearNamed;
+
+    public abstract String getMove();
+    public abstract String getBreath();
+    public abstract String getReproduce();
+
+}
