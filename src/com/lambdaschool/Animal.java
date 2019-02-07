2b306934646d6c07118db8d5e46b83f43d3e7be9
Ilho Kim <isagum@gmail.com>
2019-02-06 22:39:49 -0800

    feat: continue editing abstract class Animal
    - added Animal constructor
    - unique id will given to new animal

--- src/com/lambdaschool/Animal.java
+++ src/com/lambdaschool/Animal.java
@@ -1,9 +1,17 @@
 package com.lambdaschool;
 
 public abstract class Animal {
+    static int maxId = 0;
     int idNumber;
+
     String name;
     int yearNamed;
+
+    public Animal(String name, int yearNamed) {
+        idNumber = maxId++;
+        this.name = name;
+        this.yearNamed = yearNamed;
+    }
 
     public abstract String getMove();
     public abstract String getBreath();
